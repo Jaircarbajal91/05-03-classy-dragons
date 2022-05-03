53d65a5930445698bccf90ddb21639daaa97ecb3
Daniel Lam <Dann.lam@gmail.com>
2022-05-03 14:25:00 -0700

    Added more stuff :)

--- classes/dragon.js
+++ classes/dragon.js
@@ -1,4 +1,25 @@
-// Your code here
+class Dragon {
+  constructor(name, color) {
+    this.name = name;
+    this.color = color;
+  }
+  breathesFire(){
+    return `${this.name} breathes fire everywhere! BURN!!!!`
+  }
+  static getDragons(...dragons){
+    return dragons.reduce((arr, dragon) => {
+      arr.push(dragon.name)
+      return arr
+    }, [])
+
+    //comes in as [object1, object2]
+    //iterate through the array
+    //access the keyvalue pair, to grab the name.
+    
+  }
+}
+
+
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
